90321a66e26f5cbe61ed9e10de105d986c659946
Rajan-sharma-0 <nk800109@gmail.com>
2025-05-06 23:15:58 +0530

    Initial commit

---
+++ Script.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+
+const app = express()
+app.set('view engine', 'ejs');     // Add ejs in progrma
+app.use(express.static('./'))  // Add a public file to script js to link all frontend
+
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+app.use(function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
+}
+)
+
+
+app.listen(3000)
